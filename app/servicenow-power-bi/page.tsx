9c5f3fa3107bff4033e48914f2f6197eca380c9e
Volkan Sari <sarivolkan08@gmail.com>
2026-09-13 23:03:07 +0300

    Use shared header on ITSM Analytics page3

--- app/servicenow-power-bi/page.tsx
+++ app/servicenow-power-bi/page.tsx
@@ -68,7 +68,7 @@ export default function ServiceNowPowerBIPage() {
           HEADER
       ====================================================== */}
 
-   
+   <Header active="servicenow" />
 
       {/* ======================================================
           HERO
